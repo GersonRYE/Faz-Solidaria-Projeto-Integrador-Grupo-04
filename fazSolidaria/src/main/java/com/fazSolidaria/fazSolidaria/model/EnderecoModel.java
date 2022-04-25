86431b53c8141d480e51676f5835d87f50c98b15
gerson espejo <gersonespejo127@gmail.com>
2022-04-25 18:16:38 -0300

    Atualização remoção dos getters e setters dos atributos, e aplicando a anotação @DATA implanta o getters e setter sem precisar mencionar na model

--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/EnderecoModel.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/EnderecoModel.java
@@ -14,6 +14,9 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "tb_endereco")
 public class EnderecoModel {
@@ -57,85 +60,5 @@ public class EnderecoModel {
 	@OneToMany(mappedBy = "endereco", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("endereco")
 	private List<UsuarioModel> usuario;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getLocalidade() {
-		return localidade;
-	}
-
-	public void setLocalidade(String cidade) {
-		this.localidade = cidade;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getPais() {
-		return pais;
-	}
-
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public List<UsuarioModel> getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(List<UsuarioModel> usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getUf() {
-		return uf;
-	}
-
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
 
 }
