09a969c0ae203a772b442da44899c3155b1db1a8
gerson espejo <gersonespejo127@gmail.com>
2022-04-25 23:19:13 -0300

    Atualização: alteração na referencia do JpaRepository para PedidoModel

--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/repository/PedidoRepository.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/repository/PedidoRepository.java
@@ -3,9 +3,9 @@ package com.fazSolidaria.fazSolidaria.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fazSolidaria.fazSolidaria.model.HistoricoPedidosModel;
+import com.fazSolidaria.fazSolidaria.model.PedidoModel;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<HistoricoPedidosModel, Long> {
+public interface PedidoRepository extends JpaRepository<PedidoModel, Long> {
 
 }
