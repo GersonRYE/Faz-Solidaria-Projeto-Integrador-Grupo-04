788d9cfd7fdd7294606ff510c0a8f17aa9c0b0bb
gerson espejo <gersonespejo127@gmail.com>
2022-04-25 23:06:04 -0300

    Atualização: alteração no nome da model alterado para ProdutoModel

--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/PedidoModel.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/PedidoModel.java
@@ -1,36 +1,51 @@
 package com.fazSolidaria.fazSolidaria.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
+
+@Data
 @Entity
-@Table(name = "tb_carrinho")
-public class CarrinhoModel {
+@Table(name = "tb_pedido")
+public class PedidoModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
+	private BigDecimal subTotal;
+
+	private BigDecimal valorTotal;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date datePedido = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	private UsuarioModel cliente;
+
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+	private List<ItemPedidoModel> itens;
+
 	@NotBlank(message = "A quantidade é obrigatório")
 	private int quantidade;
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
+	public void calcularValorTotal() {
+		getItens().forEach(ItemPedidoModel::calcularPrecoTotal);
+		this.subTotal = getItens().stream().map(item -> item.getPrecoTotal()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		// this.valorTotal = this.subTotal.add(this.taxaFrete);
 	}
 
 }
