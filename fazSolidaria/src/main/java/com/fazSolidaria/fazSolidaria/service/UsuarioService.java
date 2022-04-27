c80d8e7050c3b5892e5a917682c042fad6c1af0e
Juliana Azevedo <juliana_azevedo@hotmail.com.br>
2022-04-27 20:49:51 -0300

    correção do package service

--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/service/UsuarioService.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/service/UsuarioService.java
@@ -1,4 +1,4 @@
-package com.fazSolidaria.fazSolidaria.serviceUsuarioService;
+package com.fazSolidaria.fazSolidaria.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
