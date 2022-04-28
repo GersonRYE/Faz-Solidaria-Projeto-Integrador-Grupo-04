6f6ef67049cd2ace48a2d07fdd5ffd727b3eb7ee
Juliana Azevedo <juliana_azevedo@hotmail.com.br>
2022-04-28 13:40:53 -0300

    criação do relacionamento das tabelas tb_carrinho, tb_pedidos_tb_produtos

--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/CarrinhoModel.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/CarrinhoModel.java
@@ -4,8 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_carrinho") //tabela CARRINHO
@@ -16,6 +20,12 @@ public class CarrinhoModel {
 
 	@NotBlank(message = "A quantidade é obrigatório")
 	private int quantidade;
+	
+	@ManyToOne
+	private ProdutosModel produtos;
+	
+	@ManyToOne
+	private PedidosModel pedidos;
 
 	public long getId() {
 		return id;
--- fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/PedidosModel.java
+++ fazSolidaria/src/main/java/com/fazSolidaria/fazSolidaria/model/PedidosModel.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -23,6 +24,9 @@ public class PedidosModel {
 
 	@NotBlank(message = "Total do Pedido é obrigatório")
 	private double totalPedido;
+	
+	@ManyToOne
+	private ProdutosModel produtos;
 
 	public long getId() {
 		return id;
